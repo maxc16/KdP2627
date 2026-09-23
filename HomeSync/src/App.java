a28f22db4bf0fe1a952c5fc93af3d045ee9edbcd
Max Chauveau <145713424+maxc16@users.noreply.github.com>
2026-09-23 14:41:08 +0000

    personList ArrayList deklariert und initialisiert.

--- HomeSync/src/App.java
+++ HomeSync/src/App.java
@@ -1,5 +1,8 @@
+import java.util.lang.Arraylist;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        private  Arraylist<Person> personList = new Arraylist<>;
+        System.out.println(Person.length());
     }
 }
