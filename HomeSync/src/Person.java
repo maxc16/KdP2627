66f3cbf75e305db1e2732c0f10a95a9a2f8fc712
Max Chauveau <145713424+maxc16@users.noreply.github.com>
2026-09-23 14:18:10 +0000

    Konstruktur + Erweiterung um Standard-Varaiblen und getter & setter.

--- HomeSync/src/Person.java
+++ HomeSync/src/Person.java
@@ -1,8 +1,52 @@
 public class Person {
     protected String name;
+    protected int age;
+    protected int points;
+    protected int punishementCounter;
 
 
-    public Person(){
+
+    public Person(String name, int age, int points, int punishementCounter){
     this.name = name;
+    this.age = age;
+    this.points = points;
+    this.punishementCounter = punishementCounter;
+    }
+
+    // getters and setters for name, age, points, and punishementCounter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getPunishementCounter() {
+        return punishementCounter;
+    }
+
+    public void setPunishementCounter(int punishementCounter) {
+        this.punishementCounter = punishementCounter;
+    }
+
+    public void incrementPunishementCounter() {
+        this.punishementCounter++;
     }
 }
